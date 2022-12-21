7498f27df6a1d86ac5f0c3d62181a4df4922ef1d
Joshua Sandoval <joshuaisgoogling@gmail.com>
2022-12-20 21:20:48 -0700

    Added "canonicalURL" filter.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
-module.exports = {
-    dir: {
-        input: "_11ty",
-        layouts: "_layouts"
-    }
+// Require 11ty site metadata.
+const site = require("./_11ty/_data/site.js");
+
+module.exports = function (eleventyConfig) {
+    // Add filter that converts URLs into a canonical version:
+	eleventyConfig.addFilter("canonicalURL", (value) => {
+		return [site.url, value].join("/").replace(/([^:]\/)\/+/g, "$1");
+	});
+	// Customize input and layout directories.
+	return {
+        dir: {
+            input: "_11ty",
+            layouts: "_layouts"
+        }
+    };
 };
---
+++ _11ty/_data/site.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "title": "Hey, Jes!",
+    "url": "https://heyjes.dev/",
+    "github": "https://github.com/JoshuaSand0val/"
+};
