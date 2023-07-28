7fae68e0c6fb7513038922871d60316ed90925d4
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-07-27 21:57:40 -0600

    Created basic route for Home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 // App: Component containing the entire website.
 
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./layouts/Layout"
+import Home from "./pages/Home";
+
+import "./styles/App.scss";
+
 export default function App() {
-	return <></>;
+	return (
+		<Routes>
+			<Route element={<Layout />}>
+				<Route path="/" element={<Home />} />
+			</Route>
+		</Routes>
+	);
 }
