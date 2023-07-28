495ebc73a9d62d7b1d8e7e9cd30c863188b0fcfe
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-07-27 21:21:32 -0600

    Added NavList component.

---
+++ src/components/NavList.jsx
@@ -0,0 +1,15 @@
+// Navigation List: A horizontally wrapping list of links.
+
+import { NavLink } from "react-router-dom";
+
+import "../styles/NavList.scss";
+
+export default function NavList({ links }) {
+	return (
+		<nav className="NavList">
+			{Object.keys(links).map(title => {
+				return <NavLink to={links[title]}>{title}</NavLink>;
+			})}
+		</nav>
+	);
+}
