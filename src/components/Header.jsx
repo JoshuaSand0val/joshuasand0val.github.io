1a22d946e05cc26d3b61baa78237c6275b95aca2
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-10-25 19:08:06 -0600

    Added banner image to Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import "../styles/Header.scss";
 export default function Header({ title, subtitle, navigation }) {
 	return (
 		<header className="header">
+			<img className="header-image" src="/images/hero/banner.jpg" alt="banner image" loading="lazy" />
 			<div className="header-wrapper">
 				<NavList links={navigation} />
 				<h2 className="header-title">
