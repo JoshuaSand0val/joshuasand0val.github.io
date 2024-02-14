95b467fddfabf94e620c84831d218ce87edf0c05
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-02-14 11:33:49 -0700

    Added markup and styles for default layout.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import "../styles/app.css";
+</script>
+
+<main>
+	<slot></slot>
+</main>
+
+<style>
+	main {
+		display: block;
+		container: main / inline-size;
+		max-width: var(--width-3xl);
+		border-width: var(--safe-main);
+		border-style: solid;
+		border-color: transparent;
+		padding: var(--space-s-2xl);
+		margin: 0 auto;
+	}
+</style>
