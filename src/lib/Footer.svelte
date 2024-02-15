e5d70dd81459046c0053fb7de1456695979257f5
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-02-14 22:35:24 -0700

    Made footer top of page link only display when needed.

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,24 @@
 <script>
 	import "../styles/root.css";
+    import { onMount } from "svelte";
+
+	/** @type {boolean} Boolean for if there is a visible vertical scrollbar. */
+	let scrollbarVisible = false;
+	onMount(() => {
+		/** @type {HTMLElement} Root HTML element. */
+		const root = document.documentElement;
+
+		/** Listener for if scrollbar is visible. */
+		const scrollbarListener = () => {
+			scrollbarVisible = root.scrollHeight > root.offsetHeight;
+		};
+
+		// Initially run scrollbar listener:
+		scrollbarListener();
+
+		// Watch for scrollbar visibility when resizing window:
+		window.addEventListener("resize", scrollbarListener);
+	});
 </script>
 
 <footer class="container">
@@ -8,7 +27,7 @@
 			<span>&copy; {new Date().getFullYear()} Joshua Elijah Sandoval.</span>
 			<span>Site built with <a target="_blank" href="https://svelte.dev">Svelte</a>.</span>
 		</div>
-		<a href="#top" aria-label="To top of page" class="top">
+		<a href="#top" aria-label="To top of page" class="top" data-show={scrollbarVisible}>
 			<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
 				<path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
 			</svg>
@@ -49,6 +68,9 @@
 		bottom: var(--space-m-l);
 		font-size: var(--font-xl);
 		line-height: 1;
+		&[data-show="false"] {
+			display: none;
+		}
 		&:not(:active) {
 			transition: transform 150ms ease-out;
 		}
