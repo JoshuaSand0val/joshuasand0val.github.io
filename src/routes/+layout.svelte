359813309114c99a9feffa63e6a8d5471a087701
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-02-14 19:41:46 -0700

    Added homepage content and styles.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,13 +14,17 @@
 
 <style>
 	main {
-		display: block;
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: flex-start;
 		container: main / inline-size;
+		width: 100%;
 		max-width: var(--width-3xl);
 		border-width: var(--safe-main);
 		border-style: solid;
 		border-color: transparent;
 		padding: var(--space-2xs) var(--space-s-2xl);
+		flex: 1 0 auto;
 		margin: 0 auto;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+	<img src="pfp.jpg" alt="Joshua Elijah Sandoval" />
+	<div>
+		<h1>Hey, I'm Joshua Elijah Sandoval.</h1>
+		<h2>A full-stack web developer based in Utah.</h2>
+		<p>Welcome to my website! Here you can explore and learn more <a href="/about/">about myself</a> and my hobbies, <a href="/projects/">my projects</a>, and even <a href="/contact/">contact me</a>; if perhaps you're interested.</p>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-flow: row wrap;
+		justify-content: center;
+		align-content: center;
+		align-items: flex-start;
+		width: 100%;
+		max-width: var(--width-xl);
+		container-type: inline-size;
+		margin: auto;
+		gap: var(--space-l-xl);
+	}
+
+	img {
+		display: block;
+		aspect-ratio: 1 / 1;
+		width: var(--width-2xs);
+		border: var(--space-3xs-2xs) solid currentColor;
+		border-radius: 49% 51% 48% 52% / 57% 44% 56% 43%;
+		object-fit: cover;
+		flex: 0 1 33%;
+	}
+
+	div {
+		max-width: 100%;
+		container-type: inline-size;
+		flex: 1 0 var(--width-m);
+	}
+
+	h1 {
+		font-size: var(--font-4xl);
+		font-weight: 800;
+		margin-top: 0;
+	}
+
+	h2 {
+		font-size: var(--font-xl);
+		font-weight: 300;
+		margin-bottom: var(--space-s-2xl);
+	}
+
+	p {
+		font-size: var(--font-l);
+	}
+
+	a {
+		font-family: var(--font-brand);
+		font-size: var(--font-xl);
+	}
+</style>
