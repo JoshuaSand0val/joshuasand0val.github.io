180eb04b2b7f8f52c1eccd64ee1e183a5071e215
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-02-19 18:13:32 -0700

    Added about page.

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,53 @@
+<article>
+	<h1>My name is Joshua Elijah Sandoval.</h1>
+	<h2>I am a full-stack web developer.</h2>
+	<div class="container">
+		<p>
+			I started my web development journey <strong>back in 2011</strong> after being inspired by
+			the creations of popular YouTube video channels at the time, alongside my father's notice.
+			He himself being a software architect <b>specializing in the C++ programming language.</b>
+		</p>
+		<p>
+			I quickly took an interest to <b>CSS</b>, the technology that builds the internet visually.
+			Since its introduction to me; <strong>it rose to be my absolute favorite.</strong>
+			I have since spent my years of experience studying and specializing in it.
+			And have even developed my own design ideology harmonious to the craft.
+			<strong>If I can visualize it, I can build it.</strong>
+		</p>
+		<p>
+			During my years of learning and labour, <strong>I have seen the industry shift from technology to technology.</strong>
+			In that ever-growing madness, I have come to appreciate simplicity.
+			The importance of having a strong grasp of fundamentals cannot be understated.
+			It allows one to <strong>quickly adapt to new and unknown challenges</strong> congruent to their existing skills.
+			In this case, the trio of programming languages comprising the web: <strong>HTML, CSS, and JavaScript</strong>.
+		</p>
+		<p>
+			Running into mid 2023, in pursuit of mine own betterment;
+			<strong>I decided to further my education in that of
+			<a href="https://devmountain.com/" target="_blank">Devmountain</a>.</strong>
+			Beside a cohort of fellow learners and developers, I mastered the art and
+			gained confidence and invaluable insight into the profession.
+		</p>
+		<p>
+			In the now present, I spend my time learning more.
+			<strong>Always ready and able to overcome any new challenges pertaining to web development.</strong>
+		</p>
+	</div>
+</article>
+
+<style>
+	h1 {
+		font-size: var(--font-3xl);
+		margin: 0;
+	}
+
+	h2 {
+		font-size: var(--font-l);
+		margin-top: var(--space-2xs-xs);
+	}
+
+	.container {
+		display: block;
+		max-width: var(--width-l);
+	}
+</style>
