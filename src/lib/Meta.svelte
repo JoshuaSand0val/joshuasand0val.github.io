bdf62c448f6698bdb948c3bf7c144c74027c3130
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-02-21 22:13:13 -0700

    Added Open Graph metadata.

--- src/lib/Meta.svelte
+++ src/lib/Meta.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { page } from "$app/stores";
 	/** @type {string} - Title of current webpage. */
 	export let title;
 	/** @type {string} - Description of current webpage. */
@@ -8,4 +9,10 @@
 <svelte:head>
 	<title>{title} | Hey, Jes!</title> 
 	<meta name="description" content={description} />
+	<!-- Open Graph: -->
+	<meta property="og:site_name" content="Hey, Jes!" />
+	<meta property="og:title" content={title} />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="/opengraph.png" />
+	<meta property="og:url" content="https://heyjes.dev{$page.url.pathname}" />
 </svelte:head>
