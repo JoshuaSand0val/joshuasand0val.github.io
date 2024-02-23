acc7b1e1623c24a877e730237fdd0aef6f0721da
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-02-23 13:55:53 -0700

    Redesigned project page cards.

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import "bootstrap-icons/font/bootstrap-icons.min.css";
 	import projects from "./projects.json";
 	import Meta from "$lib/Meta.svelte";
 </script>
@@ -20,13 +21,18 @@
 				{:else if typeof image === "string"}
 				<img src={image} loading="lazy" alt="" />
 				{:else}
-				<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" viewBox="-16 -16 48 48">
+				<svg fill="currentColor" viewBox="-16 -16 48 48">
 					<path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
 					<path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
 				</svg>
 				{/if}
 			</a>
 			<p class="description">{description}</p>
+			<span class="learn-more">
+				<a href={url} target="_blank">
+					Learn More <i class="bi bi-chevron-right"></i>
+				</a>
+			</span>
 		</article>
 		{/each}
 	</div>
@@ -38,51 +44,56 @@
 		font-size: var(--font-3xl);
 		margin: 0;
 	}
-	
+
 	.grid {
 		--gap: var(--space-m-l);
 		--min: var(--width-xs);
-		--max: calc(100% / 4);
+		--max: calc(100% / 3);
 		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(clamp(calc(var(--max) - var(--gap)), var(--min), 100%), 1fr));
+		grid-template-columns: repeat(auto-fill, minmax(clamp(var(--max) - var(--gap), var(--min), 100%), 1fr));
+		align-items: stretch;
 		gap: var(--gap);
 		margin: var(--space-m-l) 0;
 	}
 
 	.project {
-		display: block;
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: flex-start;
 		container-type: inline-size;
-		filter: drop-shadow(0 2px 4px rgba(0, 0, 0, 0.25));
-		.title, .portal, .description {
-			margin: var(--space-xs-s) 0 0;
-			&:first-child {
-				margin-top: 0;
-			}
+		border-radius: 3px;
+		background-color: var(--bg-color-1);
+		box-shadow: 0 4px 16px -12px black;
+		padding: var(--space-s) 0;
+		gap: var(--space-xs-s) 0;
+		.title, .portal, .description, .learn-more {
+			display: block;
+			font-size: var(--font-m);
+			margin: 0 var(--space-s);
 		}
 		.title {
-			display: block;
 			font-size: var(--font-2xl);
 		}
 		.portal {
 			position: relative;
-			display: block;
-			width: 100%;
+			z-index: var(--z-overlay);
 			aspect-ratio: 3 / 2;
-			border-radius: 3px;
-			overflow: hidden;
-			margin-right: 0;
-			margin-left: 0;
-			transition: transform 150ms ease-out;
-			background-color: var(--bg-color-1);
-			background-image: linear-gradient(123deg, var(--bg-color-2), var(--bg-color-1));
+			width: 100%;
+			margin: 0;
+			transition: transform 100ms ease-out;
+			background-color: var(--bg-color-2);
+			background-image: radial-gradient(transparent, var(--bg-color-1));
 			color: var(--color);
+			overflow: hidden;
 			&:hover, &:focus {
-				transform: scale(1.02);
+				border-radius: inherit;
+				transform: scale(1.03);
+				color: var(--color-accent);
 			}
 			&, &:active {
 				transform: none;
 			}
-			img {
+			img, svg {
 				display: block;
 				width: 100%;
 				height: 100%;
@@ -90,9 +101,11 @@
 				object-position: top center;
 			}
 		}
-		.description {
-			display: block;
-			font-size: var(--font-m);
+		.learn-more {
+			text-align: right;
+			border-top: 1px solid var(--bg-color-2);
+			padding-top: inherit;
+			margin-top: auto;
 		}
 	}
 </style>
