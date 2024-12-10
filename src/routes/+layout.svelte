34735ae7f2c917d982cc2dad2ac564f136863ec1
Joshua Sandoval <joshuaisgoogling@gmail.com>
2024-12-10 09:42:56 -0700

    Added page background gradient.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,6 +26,18 @@
 		/* Line Heights: */
 		--line-height: 1.75;
 		--display-line-height: 1.25;
+		/* Color Weights: */
+		--neutral-50: oklch(0.95 0.03 310);
+		--neutral-100: oklch(0.9 0.03 310);
+		--neutral-200: oklch(0.8 0.03 310);
+		--neutral-300: oklch(0.7 0.03 310);
+		--neutral-400: oklch(0.6 0.03 310);
+		--neutral-500: oklch(0.5 0.03 310);
+		--neutral-600: oklch(0.4 0.03 310);
+		--neutral-700: oklch(0.3 0.03 310);
+		--neutral-800: oklch(0.2 0.03 310);
+		--neutral-900: oklch(0.1 0.03 310);
+		--neutral-950: oklch(0.05 0.03 310);
 		/** Light-Dark Support: */
 		@supports (color: light-dark(white, black)) {
 			color-scheme: light dark;
@@ -40,6 +52,9 @@
 		font-family: var(--font);
 		line-height: var(--line-height);
 		background-color: var(--neutral-50-800);
+		background-image: linear-gradient(145deg, var(--neutral-50-800), var(--neutral-100-900));
+		background-size: 100% 100%;
+		background-attachment: fixed;
 		color: var(--neutral-900-50);
 		scroll-behavior: smooth;
 		transition-property: background-color, color;
