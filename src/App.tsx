494700d9c7db1428e192b1623e27f120f7599797
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-01-11 02:54:36 -0700

    Added App component inners.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-/** The website application component. */
+import styles from "./App.module.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
+/** The website application component. */
 export default function App() {
 	return (
-		<></>
+		<article className={styles.container}>
+			<Header />
+			<main className={styles.content}>
+			</main>
+			<Footer />
+		</article>
 	);
 }
