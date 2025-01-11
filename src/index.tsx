113ff36317704294f3f45491c6eaa5b340a34f45
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-01-11 01:17:48 -0700

    Renamed main.tsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+/** The website HTML index. */
+
 import "sanitize.css";
 import "semantic-props";
+import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
