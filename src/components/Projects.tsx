70a2a27b67e4febd9326f421af29d0982aeff44f
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-01-12 15:28:37 -0700

    Tweaked website design elements.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,20 +15,22 @@ export default function Projects() {
 	return (
 		<div className={styles.grid}>
 			{projects.map(({ title, image, description, url }: projects) => (
-			<div className={styles.item}>
+				<div className={styles.item}>
 					<article className={styles.project}>
-						<h2 className={styles.title}>{title}</h2>
+						<header className={styles.header}>
+							<h2 className={styles.title}>{title}</h2>
+							<p className={styles.description}>{description}</p>
+						</header>
 						{typeof image === "string" && (
 							<a className={styles.portal} href={url}>
 								<img src={image} loading="lazy" alt="" />
 							</a>
 						)}
-						<p className={styles.description}>{description}</p>
 						<span className={styles.link}>
 							<Link to={url} content="Learn More" />
 						</span>
 					</article>
-			</div>
+				</div>
 			))}
 		</div>
 	);
