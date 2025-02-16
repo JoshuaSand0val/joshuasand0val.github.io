4aa11b420d2ef891b35ac9cb8feaf8f7e3226621
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-02-16 11:58:55 -0700

    Improved navigation menu.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,23 @@
+import { useId } from "react";
 import styles from "./Header.module.css";
 import Link from "./Link";
 
 /** The website main navigation header. */
 export default function Header() {
+	const navId = useId();
+
 	return (
 		<header className={styles.container}>
 			<h4 className={styles.title}>
 				<Link to="/" content="Hey, Jes!" />
 			</h4>
-			<div className={styles.backdrop} />
-			<button className={styles.navigationButton} tabIndex={0}>
+			<button className={styles.navigationButton} tabIndex={-1} onClick={() => document.getElementById(navId)!.focus()}>
 				<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
 					<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
 				</svg>
 			</button>
-			<nav className={styles.navigation} tabIndex={0}>
+			<div className={styles.backdrop} />
+			<nav className={styles.navigation} id={navId} tabIndex={-1}>
 				<a className={styles.optionLink} href="#projects">Projects</a>
 				<a className={styles.optionLink} href="https://github.com/joshuasand0val">Github</a>
 				<a className={styles.optionLink} href="https://linkedin.com/in/heyjes/">Linkedin</a>
