4842dd86faed4b32fa3f2fd41674feaeb1a5867f
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-21 03:30:07 -0700

    Added "GlassCard" effect to Window component.

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import styles from "./Window.module.css";
+import { GlassCard } from "@developer-hub/liquid-glass";
 
 type props = {
 	children: React.ReactNode
@@ -12,11 +13,15 @@ export default function Window({ children }: props) {
 
 	return (
 		<div className={styles.container} style={{"--hue": windowHue} as React.CSSProperties}>
-			<div className={styles.wrapper}>
+			<GlassCard
+				displacementScale={50}
+				blurAmount={0.1}
+				className={styles.wrapper}
+				shadowMode={false}>
 				<div className={styles.content}>
 					{children}
 				</div>
-			</div>
+			</GlassCard>
 		</div>
 	);
 }
