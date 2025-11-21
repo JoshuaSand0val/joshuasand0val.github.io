362aa7b09b3e24c40e0822f085ee183995e58087
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-21 03:41:12 -0700

    Revert "Added "GlassCard" effect to Window component."
    This reverts commit 4842dd86faed4b32fa3f2fd41674feaeb1a5867f.

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,6 +1,5 @@
 import { useMemo } from "react";
 import styles from "./Window.module.css";
-import { GlassCard } from "@developer-hub/liquid-glass";
 
 type props = {
 	children: React.ReactNode
@@ -13,15 +12,11 @@ export default function Window({ children }: props) {
 
 	return (
 		<div className={styles.container} style={{"--hue": windowHue} as React.CSSProperties}>
-			<GlassCard
-				displacementScale={50}
-				blurAmount={0.1}
-				className={styles.wrapper}
-				shadowMode={false}>
+			<div className={styles.wrapper}>
 				<div className={styles.content}>
 					{children}
 				</div>
-			</GlassCard>
+			</div>
 		</div>
 	);
 }
