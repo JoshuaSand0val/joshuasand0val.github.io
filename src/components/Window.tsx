05395a245321292eac9cd580e840350c42b8c53a
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-21 05:20:33 -0700

    Added dark and hue props to Window component.

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -2,16 +2,21 @@ import { useMemo } from "react";
 import styles from "./Window.module.css";
 
 type props = {
-	children: React.ReactNode
+	children: React.ReactNode,
+	dark?: boolean,
+	hue?: number
 };
 
 /** Translucent, bordered container for content. */
-export default function Window({ children }: props) {
+export default function Window({ children, dark = false, hue }: props) {
+	/** Dark-theme CSS class name. */
+	const darkClass: string = dark ? styles.dark : "";
+
 	/** Degrees value of randomly selected window hue. */
-	const windowHue: number = useMemo(() => Math.floor(Math.random() * 360), []);
+	const windowHue: number = useMemo(() => hue ?? Math.floor(Math.random() * 360), []);
 
 	return (
-		<div className={styles.container} style={{"--hue": windowHue} as React.CSSProperties}>
+		<div className={`${styles.container} ${darkClass}`} style={{"--hue": windowHue} as React.CSSProperties}>
 			<div className={styles.wrapper}>
 				<div className={styles.content}>
 					{children}
