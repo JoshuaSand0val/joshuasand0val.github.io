7a5ece5be7c505d755728e4d0b2de4684c1f6a2f
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-21 19:58:47 -0700

    Removed default border-radius on Window. Added className Window prop.

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -2,13 +2,14 @@ import { useMemo } from "react";
 import styles from "./Window.module.css";
 
 type props = {
+	className?: string,
 	children: React.ReactNode,
 	dark?: boolean,
 	hue?: number
 };
 
 /** Translucent, bordered container for content. */
-export default function Window({ children, dark = false, hue }: props) {
+export default function Window({ className = "", children, dark = false, hue }: props) {
 	/** Dark-theme CSS class name. */
 	const darkClass: string = dark ? styles.dark : "";
 
@@ -16,12 +17,14 @@ export default function Window({ children, dark = false, hue }: props) {
 	const windowHue: number = useMemo(() => hue ?? Math.floor(Math.random() * 360), []);
 
 	return (
-		<div className={`${styles.container} ${darkClass}`} style={{"--hue": windowHue} as React.CSSProperties}>
+		<article
+			className={`${styles.container} ${darkClass} ${className}`}
+			style={{ "--hue": windowHue } as React.CSSProperties}>
 			<div className={styles.wrapper}>
 				<div className={styles.content}>
 					{children}
 				</div>
 			</div>
-		</div>
+		</article>
 	);
 }
