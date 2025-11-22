340a1c29a279539ff73bd87f39b48966e53d7ea6
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-21 20:24:38 -0700

    Removed dark prop from Window.

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -4,21 +4,17 @@ import styles from "./Window.module.css";
 type props = {
 	className?: string,
 	children: React.ReactNode,
-	dark?: boolean,
 	hue?: number
 };
 
 /** Translucent, bordered container for content. */
-export default function Window({ className = "", children, dark = false, hue }: props) {
-	/** Dark-theme CSS class name. */
-	const darkClass: string = dark ? styles.dark : "";
-
+export default function Window({ className = "", children, hue }: props) {
 	/** Degrees value of randomly selected window hue. */
 	const windowHue: number = useMemo(() => hue ?? Math.floor(Math.random() * 360), []);
 
 	return (
 		<article
-			className={`${styles.container} ${darkClass} ${className}`}
+			className={`${styles.container} ${className}`}
 			style={{ "--hue": windowHue } as React.CSSProperties}>
 			<div className={styles.wrapper}>
 				<div className={styles.content}>
