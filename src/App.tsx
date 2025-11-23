f5679f5181d5f28a27957555b1d27b6dab7df023
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-22 23:56:28 -0700

    Setup base website styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TintedGlass from './components/TintedGlass'
+import styles from "./App.module.css";
+import TintedGlass from "./components/TintedGlass";
 
-function App() {
-	const [count, setCount] = useState(0)
-
+export default function App() {
 	return (
-		<TintedGlass>
-			<div className="container">
-				<div>
-					<a href="https://vite.dev" target="_blank">
-						<img src={viteLogo} className="logo" alt="Vite logo" />
-					</a>
-					<a href="https://react.dev" target="_blank">
-						<img src={reactLogo} className="logo react" alt="React logo" />
-					</a>
-				</div>
-				<h1>Vite + React</h1>
-				<div className="card">
-				<TintedGlass>
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</button>
-				</TintedGlass>
-				<p>Edit <code>src/App.tsx</code> and save to test HMR</p>
-				</div>
-				<p className="read-the-docs">
-					Click on the Vite and React logos to learn more
-				</p>
-			</div>
-		</TintedGlass>
-	)
+		<div className={styles.container}>
+			<TintedGlass className={styles.content} hue={10}>
+				...
+			</TintedGlass>
+		</div>
+	);
 }
-
-export default App;
