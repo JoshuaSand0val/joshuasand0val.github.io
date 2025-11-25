bbe87cb809825fedd543c86894f56580343ece0c
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-25 00:38:53 -0700

    Added MenuBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import styles from "./App.module.css";
+import MenuBar from "./components/MenuBar";
 
 export default function App() {
 	return (
 		<div className={styles.container}>
+			<MenuBar />
 			<main className={styles.content}>
 			</main>
 		</div>
