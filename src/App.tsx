5e987a11492e77439d54b3498f6f0bc1ee2a3bff
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-24 21:00:49 -0700

    Styled App component background.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import styles from "./App.module.css";
-import TintedGlass from "./components/TintedGlass";
 
 export default function App() {
 	return (
 		<div className={styles.container}>
-			<TintedGlass className={styles.content} hue={10}>
-				...
-			</TintedGlass>
+			<main className={styles.content}>
+			</main>
 		</div>
 	);
 }
