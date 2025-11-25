cbf476c289be25c76124e7d21e5f3f10ef9b0970
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-25 15:08:42 -0700

    Fixed MenuBar overflow.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -4,34 +4,36 @@ import TintedGlass from "./TintedGlass";
 export default function MenuBar() {
 	return (
 		<header className={styles.container}>
-			<TintedGlass>
-				<nav className={styles.nav}>
-					<a href="/" className={`${styles.link} ${styles.active}`}>
-						<span className={styles.icon}>
-							<i className="bi bi-house-door"></i>
-						</span>
-						<span className={styles.label}>Hey, Jess!</span>	
-					</a>
-					<a href="/about" className={styles.link}>
-						<span className={styles.icon}>
-							<i className="bi bi-book"></i>
-						</span>
-						<span className={styles.label}>About</span>
-					</a>
-					<a href="/software" className={styles.link}>
-						<span className={styles.icon}>
-							<i className="bi bi-code-slash"></i>
-						</span>
-						<span className={styles.label}>Code</span>	
-					</a>
-					<a href="/contact" className={styles.link}>
-						<span className={styles.icon}>
-							<i className="bi bi-person"></i>
-						</span>
-						<span className={styles.label}>Contact</span>
-					</a>
-				</nav>
-			</TintedGlass>
+			<div className={styles.wrapper}>
+				<TintedGlass>
+					<nav className={styles.nav}>
+						<a href="/" className={`${styles.link} ${styles.active}`}>
+							<span className={styles.icon}>
+								<i className="bi bi-house-door"></i>
+							</span>
+							<span className={styles.label}>Hey, Jess!</span>	
+						</a>
+						<a href="/about" className={styles.link}>
+							<span className={styles.icon}>
+								<i className="bi bi-book"></i>
+							</span>
+							<span className={styles.label}>About</span>
+						</a>
+						<a href="/software" className={styles.link}>
+							<span className={styles.icon}>
+								<i className="bi bi-code-slash"></i>
+							</span>
+							<span className={styles.label}>Code</span>	
+						</a>
+						<a href="/contact" className={styles.link}>
+							<span className={styles.icon}>
+								<i className="bi bi-person"></i>
+							</span>
+							<span className={styles.label}>Contact</span>
+						</a>
+					</nav>
+				</TintedGlass>
+			</div>
 		</header>
 	);
 }
