87182286df00000091a0d26cba297bb6c9c7a415
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-11-27 02:44:31 -0700

    Renamed profile art image file.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -28,7 +28,7 @@ export default function Profile({ src, description }: props) {
 		display: block;
 		inline-size: 100%;
 		block-size: 100%;
-		background-image: url("profile-art.jpg");
+		background-image: url("art.jpg");
 		background-size: cover;
 		background-position: center;
 		object-fit: cover;
