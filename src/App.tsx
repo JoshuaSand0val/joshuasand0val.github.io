d9c0eacabeacc30006f1d7037e1f2304fdb485a7
Joshua Sandoval <joshuaisgoogling@gmail.com>
2025-12-02 00:21:01 -0700

    Made App background-image stripe thinner.

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export default function App() {
 		word-spacing: var(--normal-word);
 		letter-spacing: var(--normal-letter);
 		background-color: var(--background-color);
-		background-image: linear-gradient(to bottom, transparent 50%, var(--middleground-color) 50%);
+		background-image: linear-gradient(to bottom, transparent 80%, var(--middleground-color) 80%);
 		background-size: 100% var(--3x-small);
 		background-repeat: repeat-y;
 		color: var(--high-contrast-color);
